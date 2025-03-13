81b5e582a33e11b2c210fae7450f9a8b37f0e500
CWD273 <itscwd273@gmail.com>
2025-03-13 16:45:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Expose-Headers', '*');
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    next();
+});
+
 app.get('/proxy', (req, res) => {
     let targetUrl = req.query.url;
     if (!targetUrl) {
         return res.status(400).send('Error: URL parameter is required');
     }
 
-    // If the target URL is relative, prepend the last known domain
     if (!/^https?:\/\//i.test(targetUrl)) {
         const lastDomain = req.headers['referer'] && new URL(req.headers['referer']).searchParams.get('url');
         if (lastDomain) {
@@ -13,15 +27,19 @@ app.get('/proxy', (req, res) => {
         }
     }
 
-    // Forward the request to the target URL
     request({ url: targetUrl, headers: { 'User-Agent': req.headers['user-agent'] } })
         .on('response', (response) => {
             delete response.headers['x-frame-options'];
-            response.headers['Access-Control-Allow-Origin'] = '*'; // Add CORS header
+            response.headers['Access-Control-Allow-Origin'] = '*';
         })
         .on('error', (error) => {
             console.error('Error forwarding the request:', error);
             res.status(500).send('Failed to fetch the requested resource.');
         })
         .pipe(res);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Proxy server is running on port ${PORT}`);
 });
