1ca59e3dc837d59d57f5debc3f90e0457a8d4d93
CWD273 <itscwd273@gmail.com>
2025-10-07 17:19:33 -0500

    Update render.js

--- render.js
+++ render.js
@@ -2,25 +2,44 @@ const puppeteer = require('puppeteer');
 const express = require('express');
 const app = express();
 
+// Add CORS middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('/', async (req, res) => {
   const targetUrl = req.query.url;
   if (!targetUrl) return res.status(400).send('Missing "url" parameter.');
-
+  
   try {
     const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      headless: 'new', // Use new headless mode
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage', // Important for Render
+        '--disable-gpu'
+      ]
     });
-
+    
     const page = await browser.newPage();
-    await page.goto(targetUrl, { waitUntil: 'networkidle2' });
-
+    
+    // Set a reasonable timeout
+    await page.goto(targetUrl, { 
+      waitUntil: 'networkidle2',
+      timeout: 30000 
+    });
+    
     const html = await page.content();
     await browser.close();
-
+    
     res.setHeader('Content-Type', 'text/html');
     res.send(html);
   } catch (err) {
+    console.error('Proxy error:', err);
     res.status(500).send('Error: ' + err.message);
   }
 });
